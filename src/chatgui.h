fe73325f2bc5241bc42ea06d3cf1e3a0fb260344
Lincoln Stein <lincolntstein@gmail.com>
2021-01-13 13:26:55 +0000

    Task1: Use unique_ptr for exclusive ownership

--- src/chatgui.h
+++ src/chatgui.h
@@ -2,7 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
-
+#include <memory>
 class ChatLogic; // forward declaration
 
 // middle part of the window containing the dialog between user and chatbot
@@ -15,8 +15,8 @@ private:
 
     //// STUDENT CODE
     ////
-
-    ChatLogic *_chatLogic;
+    //Task1: Make _chatLogic an exclusive resource
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +27,8 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
+    //Ref to change to .get() to return standard pointer outside of "Student Code" area
 
     // events
     void paintEvent(wxPaintEvent &evt);
